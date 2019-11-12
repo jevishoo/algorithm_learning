8afee0fb531e291e6b76896c96dd0ccb9a228465
Jevis-Hoo <jevis_hoo@163.com>
2019-11-12 21:24:40 +0800

    modified course_04.py

--- course_04.py
+++ course_04.py
@@ -1,14 +1,20 @@
-# 类变量和实例变量
-class Test(object):  
-    num_of_instance = 0  
-    def __init__(self, name):  
-        self.name = name  
-        Test.num_of_instance += 1  
-  
-if __name__ == '__main__':  
-    print(Test.num_of_instance)   # 0
-    t1 = Test('jack')  
-    print(Test.num_of_instance)   # 1
-    t2 = Test('lucy')  
-    print(t1.name , t1.num_of_instance)  # jack 2
-    print(t2.name , t2.num_of_instance)  # lucy 2
+"""
+    类变量和实例变量
+"""
+
+
+class Test(object):
+    num_of_instance = 0
+
+    def __init__(self, name):
+        self.name = name
+        Test.num_of_instance += 1
+
+
+if __name__ == '__main__':
+    print(Test.num_of_instance)  # 0
+    t1 = Test('jack')
+    print(Test.num_of_instance)  # 1
+    t2 = Test('lucy')
+    print(t1.name, t1.num_of_instance)  # jack 2
+    print(t2.name, t2.num_of_instance)  # lucy 2
