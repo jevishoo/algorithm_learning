fe3814a23ec8be7769d8f7347cb9d03a8d9642d4
Jevis-Hoo <jevis_hoo@163.com>
2020-11-13 15:42:48 +0800

    upload file

--- code/BinaryTree/BinaryTree.md
+++ code/BinaryTree/BinaryTree.md
@@ -2,3 +2,5 @@
 👉👉  [`BSTCheck ★☆☆☆☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/BSTCheck.java)
 #### Question 73
 👉👉  [`PrintEdge ★★★☆☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/PrintEdge.java)
+#### Question 74
+👉👉  [`SerialByLevel ★★☆☆☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/SerialByLevel.java)
---
+++ code/BinaryTree/SerialByLevel.java
@@ -0,0 +1,84 @@
+package code.BinaryTree;
+
+import code.StackQueue.PreorderTraversal;
+
+import java.util.LinkedList;
+
+/**
+ * @Date 2020/11/12 11:13
+ * @Created by Jevis_Hoo
+ * @Description 通过层遍历实现序列化和反序列化
+ */
+public class SerialByLevel {
+    public static String serialByLevel(TreeNode head) {
+        if (head == null)
+            return "#,";
+
+        StringBuilder res = new StringBuilder(head.value + ",");
+        LinkedList<TreeNode> list = new LinkedList<>();
+        list.addLast(head);
+        while (!list.isEmpty()) {
+            TreeNode node = list.pollFirst();
+            if (node.left != null) {
+                res.append(node.left.value).append(",");
+                list.addLast(node.left);
+            } else {
+                res.append("#,");
+            }
+
+            if (node.right != null) {
+                res.append(node.right.value).append(",");
+                list.addLast(node.right);
+            } else {
+                res.append("#,");
+            }
+        }
+
+        return res.toString();
+    }
+
+
+    public static TreeNode reconByLevelString(String str) {
+        String[] nodes = str.split(",");
+        LinkedList<TreeNode> list = new LinkedList<>();
+
+        TreeNode head = new TreeNode(Integer.parseInt(nodes[0]));
+        int index = 1;
+        list.addLast(head);
+        TreeNode node;
+        while (!list.isEmpty()) {
+            node = list.pollFirst();
+            node.left = nodes[index].equals("#") ? null : new TreeNode(Integer.parseInt(nodes[index]));
+            index++;
+            node.right = nodes[index].equals("#") ? null : new TreeNode(Integer.parseInt(nodes[index]));
+            index++;
+            if (node.left != null) {
+                list.add(node.left);
+            }
+            if (node.right != null) {
+                list.add(node.right);
+            }
+        }
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.right.left.left = new TreeNode(7);
+        root.right.right.right = new TreeNode(8);
+
+        String res = serialByLevel(root);
+        res = res.substring(0, res.length() - 1);
+        System.out.println(res);// 1,2,3,4,#,5,6,#,#,7,#,#,8,#,#,#,#,
+
+        TreeNode node = reconByLevelString(res);
+        PreorderTraversal.preOrderRecur(node);
+    }
+}
