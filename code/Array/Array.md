e204f7a31b8f8a56793b32e741aeb426a9873d85
Jevis-Hoo <jevis_hoo@163.com>
2020-11-18 11:00:17 +0800

    upload file

---
+++ code/Array/Array.md
@@ -0,0 +1,2 @@
+#### Question 76
+👉👉  [`CumulativeSum ★★☆☆☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/Array/CumulativeSum.java)
---
+++ code/Array/CumulativeSum.java
@@ -0,0 +1,58 @@
+package code.Array;
+
+import java.util.HashMap;
+
+/**
+ * @Date 2020/11/18 10:12
+ * @Created by Jevis_Hoo
+ * @Description 累加和为给定值的最长子数组长度
+ */
+public class CumulativeSum {
+    //未排序正数数组中累加和为给定值的最长子数组长度
+    public static int getMaxLengthWithPositiveSortedArray(int[] arr, int k) {
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        int sum = arr[0];
+
+        while (right < arr.length) {
+            if (sum == k) {
+                maxLength = Math.max(maxLength, right - left + 1);
+                sum -= arr[left++];
+            } else if (sum < k) {
+                right++;
+                if (right == arr.length)
+                    break;
+                sum += arr[right];
+            } else {
+                sum -= arr[left++];
+            }
+        }
+
+        return maxLength;
+    }
+
+    //未排序数组中累加和为给定值的最长子数组长度
+    public static int getMaxLengthWithSortedArray(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLength = 0;
+        map.put(0, -1);// 注意
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum - k)) {
+                maxLength = Math.max(maxLength, i - map.get(sum - k));
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 3};
+        System.out.println(getMaxLengthWithPositiveSortedArray(array, 6));
+        System.out.println(getMaxLengthWithSortedArray(array, 6));
+    }
+}
