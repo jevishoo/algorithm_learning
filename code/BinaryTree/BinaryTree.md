a303c1a4b432be3a0c0b630a5c631ff69eb0a24b
Jevis-Hoo <jevis_hoo@163.com>
2020-11-21 17:05:33 +0800

    upload file

--- code/BinaryTree/BinaryTree.md
+++ code/BinaryTree/BinaryTree.md
@@ -6,7 +6,9 @@
 👉👉  [`SerialByLevel ★★☆☆☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/SerialByLevel.java)
 #### Question 75
 👉👉  [`MorrisTraversal ★★★★☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/MorrisTraversal.java)
-#### Question 77
+#### Question 77-78
 👉👉  [`CumulativeSum ★★★☆☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/CumulativeSum.java)
 
 👉👉  [`MaxBST ★★★★☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/MaxBST.java)
+#### Question 79
+👉👉  [`MaxBSTTopoSize ★★★★☆`](https://github.com/jevishoo/algorithm_learning/blob/master/code/BinaryTree/MaxBSTTopoSize.java)
---
+++ code/BinaryTree/MaxBSTTopoSize.java
@@ -0,0 +1,123 @@
+package code.BinaryTree;
+
+import java.util.HashMap;
+
+/**
+ * @Date 2020/11/21 15:20
+ * @Created by Jevis_Hoo
+ * @Description
+ */
+public class MaxBSTTopoSize {
+    public static int getMaxBSTTopoSize1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int max = maxTopo(root, root);
+        max = Math.max(max,getMaxBSTTopoSize1(root.left));
+        max = Math.max(max,getMaxBSTTopoSize1(root.right));
+        return max;
+    }
+
+    public static int maxTopo(TreeNode h, TreeNode n) {
+        if (n != null && isBSTNode(h, n, n.value)) {
+//            System.out.println(n.value);
+            return maxTopo(h, n.left) + maxTopo(h, n.right) + 1;
+        }
+        return 0;
+    }
+
+    public static boolean isBSTNode(TreeNode h, TreeNode n, int value) {
+        if (h == null) {
+            return false;
+        }
+        if (h == n) {
+            return true;
+        }
+        return isBSTNode(h.value > value ? h.left : h.right, n, value);
+    }
+
+
+    public static class Record {
+        public int left;
+        public int right;
+
+        public Record(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /*
+    拓扑贡献记录
+     */
+    public static int getMaxBSTTopoSize2(TreeNode root) {
+        HashMap<TreeNode, Record> map = new HashMap<>();
+        return posOrder(root, map);
+    }
+
+    public static int posOrder(TreeNode head, HashMap<TreeNode, Record> map) {
+        if (head == null) {
+            return 0;
+        }
+        int ls = posOrder(head.left, map);
+        int rs = posOrder(head.right, map);
+
+        modifyMap(head.left, head.value, map, true);
+        modifyMap(head.right, head.value, map, false);
+
+        Record lr = map.get(head.left);
+        Record rr = map.get(head.right);
+        int lBST = lr == null ? 0 : lr.left + lr.right + 1;
+        int rBST = rr == null ? 0 : rr.left + rr.right + 1;
+
+        map.put(head, new Record(lBST, rBST));
+
+        return Math.max(lBST + rBST + 1, Math.max(ls, rs));
+    }
+
+    private static int modifyMap(TreeNode head, int value, HashMap<TreeNode, Record> m, boolean flag) {
+        if (head == null || (!m.containsKey(head))) {
+            return 0;
+        }
+        Record r = m.get(head);
+        if ((flag && head.value > value) || ((!flag) && head.value < value)) {
+            m.remove(head);
+            return r.left + r.right + 1;
+        } else {
+            int minus = modifyMap(flag ? head.right : head.left, value, m, flag);
+            if (flag) {
+                r.right = r.right - minus;
+            } else {
+                r.left = r.left - minus;
+            }
+            m.put(head, r);
+            return minus;
+        }
+    }
+
+
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(21);
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(3);
+
+        root.right = new TreeNode(12);
+        root.right.left = new TreeNode(10);
+        root.right.left.left = new TreeNode(4);
+        root.right.left.left.left = new TreeNode(2);
+        root.right.left.left.right = new TreeNode(5);
+        root.right.left.right = new TreeNode(14);
+        root.right.left.right.left = new TreeNode(11);
+        root.right.left.right.right = new TreeNode(15);
+        root.right.right = new TreeNode(13);
+        root.right.right.left = new TreeNode(20);
+        root.right.right.right = new TreeNode(16);
+
+        int size1 = getMaxBSTTopoSize1(root);
+        System.out.println("max size1 is " + size1);
+        int size2 = getMaxBSTTopoSize2(root);
+        System.out.println("max size2 is " + size2);
+    }
+}
