ab42122f6a08fcb48ab405aed3d9364c7e4d1e70
Jevis-Hoo <jevis_hoo@163.com>
2020-11-23 15:32:10 +0800

    add file

--- code/BinaryTree/TwoErrorNodes.java
+++ code/BinaryTree/TwoErrorNodes.java
@@ -1,9 +1,13 @@
 package code.BinaryTree;
 
+import code.StackQueue.InorderTraversal;
+
+import java.util.Stack;
+
 /**
  * @Date 2020/11/22 13:45
  * @Created by Jevis_Hoo
- * @Description 调整搜索二叉树中两个错误的节点
+ * @Description 调整搜索二叉树中两个错误的节点 在结构上完全交换两个节点的位置
  */
 public class TwoErrorNodes {
     public static TreeNode[] getTwoErrNodes(TreeNode head) {
@@ -27,7 +31,7 @@ public class TwoErrorNodes {
                         errs[0] = errs[0] == null ? pre : errs[0];
                         errs[1] = head;
                     }
-                    System.out.print(head.value + " ");
+//                    System.out.print(head.value + " ");
                     mostRight.right = null;
                     pre = head;
                     head = head.right;
@@ -38,12 +42,92 @@ public class TwoErrorNodes {
                     errs[1] = head;
                 }
                 pre = head;
-                System.out.print(head.value + " ");
+//                System.out.print(head.value + " ");
                 head = head.right;
             }
         }
-        System.out.println();
+//        System.out.println();
         return errs;
+    }
+
+    public static TreeNode[] getTwoErrParents(TreeNode head, TreeNode e1, TreeNode e2) {
+        TreeNode[] parents = new TreeNode[2];
+        if (head == null) {
+            return parents;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || head != null) {
+            if (head != null) {
+                stack.push(head);
+                head = head.left;
+            } else {
+                head = stack.pop();
+                if (head.left == e1 || head.right == e1) {
+                    parents[0] = head;
+                }
+                if (head.left == e2 || head.right == e2) {
+                    parents[1] = head;
+                }
+                head = head.right;
+            }
+        }
+        return parents;
+    }
+
+    public static TreeNode exchangeTree(TreeNode root) {
+        TreeNode[] errors = getTwoErrNodes(root);
+        TreeNode[] parents = getTwoErrParents(root, errors[0], errors[1]);
+
+        TreeNode lcTmp, rcTmp;
+        if (parents[0] == null) {
+            lcTmp = errors[1].left;
+            rcTmp = errors[1].right;
+            errors[1].left = errors[0].left;
+            errors[1].right = errors[0].right;
+            if (parents[1].left == errors[1]) {
+                parents[1].left = errors[0];
+            } else {
+                parents[1].right = errors[0];
+            }
+            errors[0].left = lcTmp;
+            errors[0].right = rcTmp;
+            return errors[1];
+        } else if (parents[1] == errors[0]) { //父子元素交换
+            lcTmp = errors[1].left;
+            rcTmp = errors[1].right;
+            if (parents[0].left == errors[0]) {
+                parents[0].left = errors[1];
+            } else {
+                parents[0].right = errors[1];
+            }
+
+            if (errors[0].left == errors[1]) {
+                errors[1].left = errors[0];
+                errors[1].right = errors[0].right;
+            }
+            errors[0].left = lcTmp;
+            errors[0].right = rcTmp;
+        } else {
+            if (parents[0].left == errors[0]) {
+                parents[0].left = errors[1];
+            } else {
+                parents[0].right = errors[1];
+            }
+            if (parents[1].left == errors[1]) {
+                parents[1].left = errors[0];
+            } else {
+                parents[1].right = errors[0];
+            }
+
+            lcTmp = errors[1].left;
+            rcTmp = errors[1].right;
+            errors[1].left = errors[0].left;
+            errors[1].right = errors[0].right;
+            errors[0].left = lcTmp;
+            errors[0].right = rcTmp;
+        }
+
+        return root;
     }
 
     public static void main(String[] args) {
@@ -56,9 +140,19 @@ public class TwoErrorNodes {
         root.right.left = new TreeNode(2);
         root.right.right = new TreeNode(7);
 
-        TreeNode[] nodes = getTwoErrNodes(root);
-        for (TreeNode node : nodes) {
+        System.out.println("Error");
+        TreeNode[] errNodes = getTwoErrNodes(root);
+        for (TreeNode node : errNodes) {
             System.out.println(node.value);
         }
+        System.out.println("Parent");
+        TreeNode[] parNodes = getTwoErrParents(root, errNodes[0], errNodes[1]);
+        for (TreeNode node : parNodes) {
+            System.out.println(node == null ? null : node.value);
+        }
+
+        System.out.println(InorderTraversal.inorderTraversal(root));
+        TreeNode head = exchangeTree(root);
+        System.out.println(InorderTraversal.inorderTraversal(head));
     }
 }
