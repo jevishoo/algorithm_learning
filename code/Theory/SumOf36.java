f8a0b551903ecbeb649f5402d7089fc5254a248c
Jevis-Hoo <jevis_hoo@163.com>
2021-05-11 08:30:29 +0800

    correct file

--- code/Theory/SumOf36.java
+++ code/Theory/SumOf36.java
@@ -18,102 +18,95 @@ public class SumOf36 {
         result = addFunWithStr("z", "z");
         //1y
         System.out.println("result = " + result);
-    }
 
-    public static int getIntFromChar(char ch) {
-
-        int ret = -1;
-
-        if (ch >= '0' && ch <= '9') {
-            ret = ch - '0';
-        } else if (ch >= 'a' && ch <= 'z') {
-
-            ret = (ch - 'a') + 10;
-        }
-
-        return ret;
-
-    }
-
-    private static int addChar(int ret, int jin, StringBuilder res) {
-        if (ret >= 36) {
-            int index = ret - 36 + jin;
-            char sv = SYM.charAt(index);
-            res.append(sv);
-            jin = 1;
-        } else {
-            int index = ret + jin;
-            char sv = SYM.charAt(index);
-            res.append(sv);
-            jin = 0;
-        }
-
-        return jin;
+        result = addFunWithStr("zx1", "92");
+        //1y
+        System.out.println("result = " + result);
     }
 
     private static final String SYM = "0123456789abcdefghijklmnopqrstuvwxyz";
 
+    public static int getIntFromChar(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return ch - '0';
+        } else {
+            // if (ch >= 'a' && ch <= 'z')
+            ch = Character.toLowerCase(ch);
+            return (ch - 'a') + 10;
+        }
+    }
+
+    private static int addChar(int sum, int jin, StringBuilder res) {
+        int index = sum + jin;
+
+        if (index >= 36) {
+            index -= 36;
+            char ch = SYM.charAt(index);
+            res.append(ch);
+            return 1;
+        } else {
+            char ch = SYM.charAt(index);
+            res.append(ch);
+            return 0;
+        }
+    }
+
+
     public static String addFunWithStr(String param1, String param2) {
+        int len1 = param1.length() - 1;
+        int len2 = param2.length() - 1;
 
-        StringBuilder res = new StringBuilder();
-        int len1 = param1.length();
-        int len2 = param2.length();
-
-        int i = len1 - 1;
-        int j = len2 - 1;
-
-        if (i < 0 || j < 0) {
-            return null;
+        if (len1 < 0 || len2 < 0) {
+            return len1 < 0 ? param2 : param1;
         }
 
+        StringBuilder res = new StringBuilder();
 
         int jin = 0;
-        while (i >= 0 && j >= 0) {
+        while (len1 >= 0 && len2 >= 0) {
 
-            char ch1 = param1.charAt(i);
-            char ch2 = param2.charAt(j);
+            char ch1 = param1.charAt(len1);
+            char ch2 = param2.charAt(len2);
 
             int v1 = getIntFromChar(ch1);
             int v2 = getIntFromChar(ch2);
 
-            int ret = v1 + v2;
+            int sum = v1 + v2;
 
-            jin = addChar(ret, jin, res);
-            i--;
-            j--;
+            jin = addChar(sum, jin, res);
+            len1--;
+            len2--;
         }
 
 
-        while (i >= 0) {
-            char ch = param1.charAt(i);
+        while (len1 >= 0) {
+            char ch = param1.charAt(len1);
 
             if (jin == 1) {
-                int index = getIntFromChar(ch) + 1;
-                jin = addChar(index, jin, res);
+                int value = getIntFromChar(ch);
+                jin = addChar(value, jin, res);
             } else {
                 res.append(ch);
             }
-            i--;
+            len1--;
         }
 
-        while (j >= 0) {
-            char ch = param2.charAt(j);
+        while (len2 >= 0) {
+            char ch = param2.charAt(len2);
 
             if (jin == 1) {
-                int index = getIntFromChar(ch) + 1;
-                jin = addChar(index, jin, res);
+                int value = getIntFromChar(ch);
+                jin = addChar(value, jin, res);
             } else {
                 res.append(ch);
             }
-            j--;
+            len2--;
         }
 
         if (jin == 1) {
             res.append(jin);
         }
 
-        StringBuilder result = res.reverse();
-
-        return result.toString();
+        return res.reverse().toString();
     }
 }
